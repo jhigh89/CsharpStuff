0503f6cb3b2e8d3339962ad555e1aaf9393cbf02
Jacob Hightower <jah795@nau.edu>
2020-07-20 20:48:21 -0700

    Created simple string matcher for second challenge. No string sanitation or anything else typically involved.

---
+++ Challenge2/Challenge2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credential = "";
+            while (credential != "secret") {
+                Console.WriteLine(" ");
+                Console.WriteLine("Please provide your case-sensitive password:");
+                credential = Console.ReadLine();
+                if (credential != "secret")
+                {
+                    Console.WriteLine("You have not been authenticated. Please provide the correct password.");
+                } 
+
+            }
+            Console.WriteLine("You have been authenticated. Please proceed!");
+
+        }
+    }
+}
