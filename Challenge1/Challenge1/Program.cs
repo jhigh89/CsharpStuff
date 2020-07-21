721ed75cf8d855f15cf683fcb8074f2099a6b0e6
Jacob Hightower <jah795@nau.edu>
2020-07-20 20:12:21 -0700

    Created tiny app for Challenge One on learning course to grab user info and spit it back out.

---
+++ Challenge1/Challenge1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, and welcome to SimpleInfoGrabber.");
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+            Console.WriteLine("Hello, {0}! You stated you are {1} years old and were born in the month of {2}.", name, age, month);
+        }
+    }
+}
