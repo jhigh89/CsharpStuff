de35a95e4437686be9819957faaed1beac744f9a
Jacob Hightower <jah795@nau.edu>
2020-07-26 17:45:10 -0700

    Did a 3rd challenge with simple for / while setup, and started another course on pluralsight that wants to go through creating a gradebook.

---
+++ Challenge3/Challenge3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Challenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loopin = 1;
+            while (loopin <= 5) {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for (int i = 10; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("There's loop {0}", loopin);
+                loopin++;
+
+            }
+        }
+    }
+}
