420814514d31043cb0433bad2ea8b55d73e513ee
Jacob Hightower <jah795@nau.edu>
2020-07-27 21:27:41 -0700

    For challenge for, had to modify school tracker to include as many students as wanted, and added a way to track grades and students.

--- SchoolTracker/SchoolTracker/Program.cs
+++ SchoolTracker/SchoolTracker/Program.cs
@@ -6,14 +6,27 @@ namespace SchoolTracker
     {
         static void Main(string[] args)
         {
-            var studentGrades = new int[] { 80,77,99,55,66,72,85,95,44,65};
-
-
-            foreach (var studentGrade in studentGrades)
-            {
-                Console.WriteLine(studentGrade);
-            }
             
+            Console.WriteLine("How many students will be in the class?");
+            var studentCount = int.Parse(Console.ReadLine());
+
+            var studentNames = new string[studentCount];
+            var studentGrades = new int[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.Write("What's the student's name?" + " ");
+                studentNames[i] = Console.ReadLine();
+                
+
+                Console.Write("What grade did they get?" + " ");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+            }
         }
-    }
+    } 
 }
