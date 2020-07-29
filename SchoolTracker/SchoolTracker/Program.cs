80fc661cdeb277de433cd47041e843795bdef4a3
Jacob Hightower <jah795@nau.edu>
2020-07-29 00:50:40 -0700

    created new class Student to record multiple variable types and clean up code.

--- SchoolTracker/SchoolTracker/Program.cs
+++ SchoolTracker/SchoolTracker/Program.cs
@@ -7,19 +7,31 @@ namespace SchoolTracker
     {
         static void Main(string[] args)
         {
-
-            var studentNames = new List<string>();
-            var studentGrades = new List<int>();
+            var students = new List<Student>();
 
             var adding = true;
 
             while ( adding == true)
             {
+                var newStudent = new Student();
+
                 Console.Write("What's the student's name?" + " ");
-                studentNames.Add(Console.ReadLine());
+                newStudent.Name = Console.ReadLine();
 
                 Console.Write("What grade did they get?" + " ");
-                studentGrades.Add(int.Parse(Console.ReadLine()));
+                newStudent.Grade = (int.Parse(Console.ReadLine()));
+
+                Console.Write("What is their birthday?" + " ");
+                newStudent.Birthday = Console.ReadLine();
+
+                Console.Write("What is their address?" + " ");
+                newStudent.Address = Console.ReadLine();
+
+                Console.Write("What is their phone number?" + " ");
+                newStudent.Phone = (int.Parse(Console.ReadLine()));
+
+                students.Add(newStudent);
+
 
                 Console.WriteLine("Add another? y/n");
                 if (Console.ReadLine() == "n")
@@ -27,10 +39,21 @@ namespace SchoolTracker
 
             }
 
-            for (int i = 0; i < studentNames.Count; i++)
+            foreach (var student in students)
             {
-                Console.WriteLine("Name: {0}, Grade: {1}", studentNames[i], studentGrades[i]);
+                Console.WriteLine("Name: {0}, Grade: {1}, Birthday: {2}, Address: {3}, Phone: {4}", student.Name, student.Grade, student.Birthday, student.Address, student.Phone);
             }
         }
-    } 
+    }
+    
+    class Student
+    {
+
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        public int Phone;
+
+    }
 }
